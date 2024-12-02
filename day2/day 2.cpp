f959acfea181e522a00eb0cde2cef60695bf3ebb
chris flinn <christopher.flinn@gmail.com>
2024-12-02 22:46:58 +1100

    update

---
+++ day2/day 2.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+auto main() -> int {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
+
+  auto count = 0;
+
+  freopen("day2_input.txt", "r", stdin);
+  std::string s;
+  while (std::getline(std::cin, s)) {
+    std::stringstream line(s);
+    auto num = int{}; auto prev = INT_MIN;
+    auto first = true; auto increase = true; auto decrease = true; auto skip = false;
+    while (line >> num) {
+      if (first) {
+        prev = num;
+        first = false;
+        continue;
+      }
+      if (num > prev) {
+        decrease = false;
+      } else if (num < prev) {
+        increase = false;
+      }
+
+      if (num == prev || abs(num - prev) > 3) {
+        skip = true;
+        break;
+      }
+      prev = num;
+    }
+
+    if (!skip && (increase || decrease)) {
+      count++;
+    }
+
+  }
+  std::cout << count << "\n";
+}
