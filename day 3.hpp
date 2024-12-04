bb33d28ddee8aa60e165f0aeca13fda7232c4749
chris flinn <christopher.flinn@gmail.com>
2024-12-05 04:46:02 +1100

    more

--- day 3.hpp
+++ day 3.hpp
@@ -8,17 +8,25 @@ namespace day_3 {
 auto solution(const char *path) -> void {
   std::ifstream file(path);
 
-  std::regex pattern(R"(^mul\(\d{1,3},\d{1,3}\)$)");
+  std::regex pattern(R"(mul\(\d{1,3},\d{1,3}\))");
+  std::regex intpattern(R"(\d{1,3})");
 
-  std::string s;
+  auto sum = 0;
+
+  auto s = std::string{};
   while (std::getline(file, s)) {
-    std::stringstream line(s);
-    auto container = std::string{};
+    auto it = std::sregex_iterator(s.begin(), s.end(), pattern);
+    auto end = std::sregex_iterator();
+    
     auto idx = int{};
-    while (line >> container) {
-      std::cout << idx << ": " << std::regex_search(container, pattern) << "\n";
+    for (; it != end; ++it) {
+      auto ss = it->str();
+      auto jt = std::sregex_iterator(ss.begin(), ss.end(), intpattern);
+      auto jend = std::sregex_iterator();
+      std::cout << idx << ": " << jt->str() << "\n";
       idx++;
     }
+
   }
 
   std::cout << "Part 1: " << "Unsolved" << "\n";
